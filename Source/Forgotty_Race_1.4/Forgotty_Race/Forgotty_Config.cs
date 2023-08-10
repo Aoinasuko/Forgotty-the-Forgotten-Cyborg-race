654faa6821ceaa8229b38cc7ae1431d6bcee3ffe
Aoi_Nasuko <aoinasuko.nasunasu@gmail.com>
2023-08-10 22:43:32 +0900

    0810

--- Source/Forgotty_Race_1.4/Forgotty_Race/Forgotty_Config.cs
+++ Source/Forgotty_Race_1.4/Forgotty_Race/Forgotty_Config.cs
@@ -29,7 +29,7 @@ namespace Forgotty_Race
 	public class Forgotty_Config : ModSettings
 	{
 		// バージョン
-		public static int ver = 200;
+		public static int ver = 210;
 
 		// アップデートバージョン
 		public static int Updatever = 0;
